a1881bfd72866a99d3b5e5691673130640571cb2
yanlinliu <yanlinliu@atop.com.tw>
2021-12-20 13:45:18 +0800

    fix bug

--- users.go
+++ users.go
@@ -3,7 +3,6 @@ package atopdb
 import (
 	"context"
 	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
 	"time"
 
@@ -31,9 +30,9 @@ func (d *DB) ValidUser(body bson.M) (string, error) {
 	if err := colleciont.FindOne(ctx, bson.M{"name": name}).Decode(&item); err != nil {
 		return "", err
 	}
-	passwrod := sha256.New().Sum(body["password"])
-
-	if item["password"] == hex.EncodeToString(passwrod[:]) {
+	//	passwrod := sha256.New().Sum(body["password"])
+	body["password"] = sha256.New().Sum(body["password"])
+	if item["password"] == body["password"] {
 		id := item["_id"].(primitive.ObjectID)
 		return id.Hex(), nil
 	}
